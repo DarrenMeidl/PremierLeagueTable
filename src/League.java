10730e0013268f6cb8b23f0d673e2b1394b12316
DarrenMeidl <dmeidl15@gmail.com>
2023-10-06 09:28:56 +0100

    Created table printing method and called it in the MainApp class

--- src/League.java
+++ src/League.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 public class League {
-    public static void main(String[] args) {
-        List<LeagueEntry> leagueEntryList = new ArrayList<>();
+    private List<LeagueEntry> leagueEntryList = new ArrayList<>();
+    public League(){
         leagueEntryList.add(new LeagueEntry());
         leagueEntryList.add(new LeagueEntry("Liverpool", 5, 5, 0, 0, 15));
-
-        printTable(leagueEntryList);
-        System.out.println(leagueEntryList);
     }
 
-    public void printTable(ArrayList<LeagueEntry> leagueEntryList){
+    //METHODS
+    //1. Prints Table
+    public void printTable() {
         System.out.printf("%17s%14s%14s%14s%14s%14s\n", "TEAM", "GAMES PLAYED", "WINS", "DEFEATS", "DRAWN", "POINTS");
+        for (int i = 0; i < leagueEntryList.size(); i++){
+            System.out.printf("%17s%14d%14d%14d%14d%14d\n",  leagueEntryList.get(i).getName(),leagueEntryList.get(i).getGamesPlayed(), leagueEntryList.get(i).getGamesWon(), leagueEntryList.get(i).getGamesLost(), leagueEntryList.get(i).getGamesDrew(), leagueEntryList.get(i).getTotalPoints());
+        }
+    }
+    //2. Plays Random Match Between 2 Teams
+    public void playMatch(){
+
     }
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,2 +1,8 @@
 public class MainApp {
+    public static void main(String[] args) {
+        League season2324 = new League();
+
+        season2324.printTable();
+        season2324.playMatch();
+    }
 }
